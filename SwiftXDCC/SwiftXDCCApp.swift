d92f32502707540f565ebda802d126d14e0d2750
Marek Skrzelowski <marek.skrzelowski@vandermesis.com>
2026-06-12 15:06:30 +0200

    Initial Commit

---
+++ SwiftXDCC/SwiftXDCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftXDCCApp.swift
+//  SwiftXDCC
+//
+//  Created by Marek Skrzelowski on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftXDCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
